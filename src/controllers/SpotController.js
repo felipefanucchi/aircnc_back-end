51ddda85260e2aaa19c26986793e081365fa4a8f
Felipe Fanucchi <felipefanucchi2010@hotmail.com>
2020-01-14 18:40:13 -0300

    Criando filtro para techs e retornando spot caso ja exista

--- src/controllers/SpotController.js
+++ src/controllers/SpotController.js
@@ -1,25 +1,37 @@
-const Spot = require("../models/Spot")
-const User = require("../models/User")
+const Spot = require("../models/Spot");
+const User = require("../models/User");
 
 module.exports = {
+  async index(request, response) {
+    const { tech } = request.query;
+
+    const spots = await Spot.find({ techs: tech });
+
+    return response.json(spots);
+  },
+
   async store(request, response) {
     const { filename: thumbnail } = request.file;
     const { company, techs, price } = request.body;
     const { user_id: user } = request.headers;
 
-    const username = User.findById(user);
+    const username = await User.findById(user);
 
     if (!username) {
       return response.status(400).json({ error: "User does not exist" });
     }
 
-    const spot = await Spot.create({
-      user,
-      thumbnail,
-      company,
-      techs: techs.split(","),
-      price
-    });
+    let spot = await Spot.findOne({ company });
+
+    if (!spot) {
+      spot = await Spot.create({
+        user,
+        thumbnail,
+        company,
+        techs: techs.split(",").map(tech => tech.trim()),
+        price
+      });
+    }
 
     return response.json(spot);
   }
